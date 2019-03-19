83409be050f5faf0590d02e7344a42b1a9e74f88
mihai <mihail.adc@gmail.com>
2019-03-19 23:47:27 +0200

    Flow working

---
+++ server/ClientHandler.py
@@ -0,0 +1,72 @@
+import socket
+import time
+from threading import Thread
+
+AUDIO_PORT_OUT = 50010
+AUDIO_PORT_IN = 50011
+
+S_STBY = 1
+S_WAIT = 2
+S_CALL = 3
+
+
+class ClientHandler(Thread):
+	
+	def __init__(self, conn):
+		Thread.__init__(self)
+		self.conn = conn
+
+		self.conn.settimeout(1.0)
+
+		self.ip, self.port = conn.getsockname()
+		self.active = True
+		self.intf_msg = ''
+		self.client_msg = ''
+		self.start()
+
+	def refreshState(self, state):
+		if state == S_STBY and self.client_msg == b'CALL':
+			state = S_WAIT
+		elif state == S_WAIT and self.intf_msg == 'RESPOND':
+			state = S_CALL
+		elif state == S_CALL and self.intf_msg == 'STOP':
+			state = S_STBY
+		return state
+
+	def respondToClient(self, state):
+		if state == S_STBY:
+			self.conn.sendall(b'STOP')
+		elif state == S_WAIT:
+			self.conn.sendall(b'HOLD')
+		elif state == S_CALL:
+			self.conn.sendall(b'RESPOND')
+
+	def doActions(self, state):
+		if state == S_STBY:
+			print('Stop thread')
+		elif state == S_WAIT:
+			print('Init thread')
+		elif state == S_CALL:
+			print('Start thread')
+
+	def run(self):
+		state = S_STBY
+		try:
+			while self.active:
+				#interf = readInterface() #il actualizeaza interfata
+				self.client_msg = ""
+				try:
+					self.client_msg = self.conn.recv(1024)
+				except socket.timeout as err:
+					pass
+				
+				state = self.refreshState(state)
+				self.respondToClient(state)
+				self.doActions(state)
+
+				print('Client handler running')
+				time.sleep(1.0)
+		finally:
+			self.conn.close()
+	def end(self):
+		self.active = False
